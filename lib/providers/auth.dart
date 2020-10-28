c0d6fd6ec2dc1511f51537bed7afbac41f365f08
lffruhling <lffruhling@hotmail.com>
2020-10-28 17:21:43 -0300

    logout automatico com timer

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
@@ -9,6 +10,7 @@ class Auth with ChangeNotifier {
   String _userId;
   String _token;
   DateTime _expiryDate;
+  Timer _logoutTimer;
 
   bool get isAuth{
     return token != null;
@@ -51,6 +53,7 @@ class Auth with ChangeNotifier {
           seconds: int.parse(responseBody['expiresIn']),
         ),
       );
+      _autoLogout();
       notifyListeners();
     }
 
@@ -69,6 +72,25 @@ class Auth with ChangeNotifier {
     _token = null;
     _userId = null;
     _expiryDate = null;
+
+    if(_logoutTimer != null){
+      _logoutTimer.cancel();
+      _logoutTimer = null;
+    }
+
     notifyListeners();
+  }
+
+  void _autoLogout(){
+    if(_logoutTimer != null){
+      _logoutTimer.cancel();
+    }
+
+    /*
+    * Pega a data de expiração e testa com a data atual e retorna em segundo o
+    * tempo de expiração do login
+    * */
+    final timeToLogout = _expiryDate.difference(DateTime.now()).inSeconds;
+    _logoutTimer= Timer(Duration(seconds: timeToLogout), logout);
   }
 }
