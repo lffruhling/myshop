b0682fa322cad1072dda492935f11f6ff58fb001
lffruhling <lffruhling@hotmail.com>
2020-10-28 18:25:01 -0300

    adiciona login automatico

--- lib/views/auth_home_screen.dart
+++ lib/views/auth_home_screen.dart
@@ -5,10 +5,21 @@ import 'package:shop/views/auth_screen.dart';
 import 'package:shop/views/products_overview_screen.dart';
 
 class AuthOrHomeScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     Auth auth = Provider.of(context);
-    return auth.isAuth ? ProductOverviewScreen() : AuthScreen();
+    return FutureBuilder(
+      future: auth.tryAuthLogin(),
+      builder: (ctx, snapshot){
+        if(snapshot.connectionState == ConnectionState.waiting){
+          return Scaffold(body: Center(child: CircularProgressIndicator()));
+        }else if( snapshot.error != null){
+          print(snapshot.error);
+          return Scaffold(body: Center(child: Text('Houve uma falha no login!')));
+        }else{
+          return auth.isAuth ? ProductOverviewScreen() : AuthScreen();
+        }
+      },
+    );
   }
 }
