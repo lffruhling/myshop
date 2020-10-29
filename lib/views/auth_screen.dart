1d8fbd5a70811e7ba4cdc9359abd305836190c2c
lffruhling <lffruhling@hotmail.com>
2020-10-29 12:57:08 -0300

    Corrige responsividade no login para quando abre o teclado

--- lib/views/auth_screen.dart
+++ lib/views/auth_screen.dart
@@ -18,44 +18,49 @@ class AuthScreen extends StatelessWidget {
               ], begin: Alignment.topLeft, end: Alignment.bottomRight),
             ),
           ),
-          Container(
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  child: FittedBox(
-                    fit: BoxFit.fitWidth,
-                    child: Text(
-                      'Minha Loja',
-                      style: TextStyle(
-                        fontFamily: 'Anton',
-                        color: Theme.of(context).accentTextTheme.headline6.color,
-                        fontSize: 45,
+          Center(
+            child: SingleChildScrollView(
+              child: Container(
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 45),
+                    Container(
+                      child: FittedBox(
+                        fit: BoxFit.fitWidth,
+                        child: Text(
+                          'Minha Loja',
+                          style: TextStyle(
+                            fontFamily: 'Anton',
+                            color: Theme.of(context).accentTextTheme.headline6.color,
+                            fontSize: 45,
+                          ),
+                        ),
                       ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.deepOrange.shade900,
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 8,
+                            color: Colors.black26,
+                            offset: Offset(0,2),
+                          ),
+                        ],
+                      ),
+                      transform: Matrix4.rotationZ(-8 * pi / 1800)..translate(-5.0),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 8,
+                        horizontal: 45,
+                      ),
+                      margin: EdgeInsets.only(bottom: 20),
                     ),
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.deepOrange.shade900,
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 8,
-                        color: Colors.black26,
-                        offset: Offset(0,2),
-                      ),
-                    ],
-                  ),
-                  transform: Matrix4.rotationZ(-8 * pi / 1800)..translate(-5.0),
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 45,
-                  ),
-                  margin: EdgeInsets.only(bottom: 20),
+                    AuthCard(),
+                  ],
                 ),
-                AuthCard(),
-              ],
+              ),
             ),
           ),
         ],
