baf89e57d0d51347d896be7be31c5f78680c2cc5
lffruhling <lffruhling@hotmail.com>
2020-10-29 15:35:54 -0300

    Customiza app bar dentro dos detalhes do produto

--- lib/views/product_detail_screen.dart
+++ lib/views/product_detail_screen.dart
@@ -8,45 +8,68 @@ class ProductDetailScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Product;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(product.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: Hero(
-                tag: 'img_product_hero_${product.id}',
-                child: Image.network(
-                  product.imageUrl,
-                  fit: BoxFit.cover,
+      // appBar: AppBar(
+      //   title: Text(product.title),
+      // ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(product.title),
+              background: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Hero(
+                    tag: 'img_product_hero_${product.id}',
+                    child: Image.network(
+                      product.imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  DecoratedBox(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment(0, 0.8),
+                            end: Alignment(0, 0),
+                            colors: [
+                          Color.fromRGBO(0, 0, 0, 0.6),
+                          Color.fromRGBO(0, 0, 0, 0),
+                        ])),
+                  )
+                ],
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                SizedBox(height: 10),
+                Container(
+                  width: double.infinity,
+                  child: Text(
+                    'R\$ ${product.price}',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 20,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
-              ),
-            ),
-            SizedBox(height: 10),
-            Container(
-              width: double.infinity,
-              child: Text(
-                'R\$ ${product.price}',
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 20,
+                SizedBox(height: 10),
+                Container(
+                  width: double.infinity,
+                  child: Text(
+                    product.description,
+                    textAlign: TextAlign.center,
+                  ),
                 ),
-                textAlign: TextAlign.center,
-              ),
+                SizedBox(height: 1000),
+              ],
             ),
-            SizedBox(height: 10),
-            Container(
-              width: double.infinity,
-              child: Text(
-                product.description,
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
